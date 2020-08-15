bf71c5cc1a458e5569df434350eec41510e92198
Mercy <aprilkasha42@gmail.com>
2020-08-15 20:07:47 +0300

    create update profile view function,create profile_update template to redirect to

--- awwards/projects/views.py
+++ awwards/projects/views.py
@@ -16,3 +16,20 @@ def profile(request):
 
     return render(request, 'projects/profile.html', locals())
 
+@login_required(login_url='/accounts/login/?next=/')
+def updateprofile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            add = form.save(commit=False)
+            add.user = current_user
+            add.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'projects/profile_update.html',{"form":form })
+
+
+
+    
