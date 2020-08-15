0610eddf4da347bb5da9f48ab9f969d92f9b0cb3
Mercy <aprilkasha42@gmail.com>
2020-08-15 20:04:39 +0300

    write a test for the profile class

--- awwards/projects/tests.py
+++ awwards/projects/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-
+from .models import Profile,Post,User
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    #Set up method
+
+    def setUp(self):
+        self.new_user = User(username='Titus', email='titusouko@gmail.com', password='1234')
+        self.new_user.save()
+        self.new_profile = Profile(user=self.new_user,profile_picture="image.jpeg",bio="just testing", contact='titusouko@gmail.com')
+    
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+    
+    def test_save_method(self):
+        self.new_profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def test_delete_method(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0)  
