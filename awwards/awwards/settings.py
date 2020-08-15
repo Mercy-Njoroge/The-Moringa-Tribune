63ce9f8c31845f5b32287a7a38eeaf423f9c82aa
Mercy <aprilkasha42@gmail.com>
2020-08-15 21:34:19 +0300

    add django.template.context_processors.media to load uploaded images in our templates

--- awwards/awwards/settings.py
+++ awwards/awwards/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'awwards.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,11 +64,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
-
 WSGI_APPLICATION = 'awwards.wsgi.application'
 
 
