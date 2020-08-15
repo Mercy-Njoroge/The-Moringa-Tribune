938048d117dd03fec0503c93d6d452489c40707c
Mercy <aprilkasha42@gmail.com>
2020-08-15 20:51:01 +0300

    link url to new post view function i created

--- awwards/projects/urls.py
+++ awwards/projects/urls.py
@@ -7,7 +7,7 @@ urlpatterns=[
     url(r'^$', views.index, name='index'),
     url(r'^update/profile$', views.updateprofile, name='update'),
     url(r'^profile/$', views.profile, name='profile'),
-    # url(r'^new/post$', views.new_post, name='new_post'),
+    url(r'^new/post$', views.new_post, name='new_post'),
     # url(r'^vote/(?P<post_id>\d+)?$', views.vote, name='vote'), 
     #  url(r'^search/', views.search_results, name='search_results'),
     # url(r'^api/post/$', views.PostList.as_view()),
