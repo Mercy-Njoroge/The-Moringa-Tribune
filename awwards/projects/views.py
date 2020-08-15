2162456e8e146fc8dedda9ac3b069ca8f5a0225a
Mercy <aprilkasha42@gmail.com>
2020-08-15 19:16:19 +0300

    create class indexto take date and posts,

--- awwards/projects/views.py
+++ awwards/projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    date =dt.date.today()
+    posts = Post.objects.all()
+    return render(request, 'projects/index.html', {"date":date, "posts":posts })
