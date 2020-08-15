d0c82bdfed7e379e53f31d51e039b7c0e3054729
Mercy <aprilkasha42@gmail.com>
2020-08-15 20:08:57 +0300

    link the url for update profiles

--- awwards/projects/urls.py
+++ awwards/projects/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.index, name='index'),
-    # url(r'^update/profile$', views.updateprofile, name='update'),
-     url(r'^profile/$', views.profile, name='profile'),
+    url(r'^update/profile$', views.updateprofile, name='update'),
+    url(r'^profile/$', views.profile, name='profile'),
     # url(r'^new/post$', views.new_post, name='new_post'),
     # url(r'^vote/(?P<post_id>\d+)?$', views.vote, name='vote'), 
     #  url(r'^search/', views.search_results, name='search_results'),
