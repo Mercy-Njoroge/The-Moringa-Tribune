a49b5e411aae50c5a355560856cae65210d5c3ae
Mercy <aprilkasha42@gmail.com>
2020-08-15 19:28:20 +0300

    import profile from models,create a profile class and profile.html template

--- awwards/projects/views.py
+++ awwards/projects/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Profile
 # Create your views here.
 def index(request):
     date =dt.date.today()
     posts = Post.objects.all()
     return render(request, 'projects/index.html', {"date":date, "posts":posts })
+
+@login_required(login_url='/accounts/login/?next=/')
+def profile(request):
+    current_user = request.user
+  profile = Profile.objects.filter(user=current_user).first()
+    posts = request.user.post_set.all()
+
+    return render(request, 'projects/profile.html', locals())
+
