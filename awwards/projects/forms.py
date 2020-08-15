85f9ae49f5b0c6b4bc883b1508d9515f57373b56
Mercy <aprilkasha42@gmail.com>
2020-08-15 20:46:09 +0300

    import post from models,create class postform which inherits from modelform

--- awwards/projects/forms.py
+++ awwards/projects/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import Profile#, Post
+from .models import Profile, Post
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude=["user"]
-# class PostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         exclude = []
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = []
