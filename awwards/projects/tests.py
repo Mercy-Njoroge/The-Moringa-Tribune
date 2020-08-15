c6c2df31b5aa794e6ee700e85f4b8c846b84bd49
Mercy <aprilkasha42@gmail.com>
2020-08-15 20:35:28 +0300

    create a test class for the post method

--- awwards/projects/tests.py
+++ awwards/projects/tests.py
@@ -23,3 +23,25 @@ class ProfileTestClass(TestCase):
         self.new_profile.delete_profile()
         profile = Profile.objects.all()
         self.assertTrue(len(profile)==0)  
+
+class PostTestClass(TestCase):
+
+    def setUp(self):
+        
+        self.new_user = User(username='Titus', email='titusouko@gmail.com', password='1234')
+        self.new_user.save()
+        self.new_post=Post(title="techs",image='image.png',description="test app stuff",user=self.new_user,link="https://w3resource.com",country="kenya")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_post,Post))    
+
+    def test_save_post(self):
+        self.new_post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post)>0)
+
+    def test_delete_post(self):
+        self.new_post.save_post()
+        self.new_post.delete_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post)==0)
